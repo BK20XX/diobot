50e3b4b20a8bb870faed0657978034aed368bd0c
BK20XX <30297207+BK20XX@users.noreply.github.com>
2017-07-19 11:56:44 -0500

    Update wry.js

--- commands/wry.js
+++ commands/wry.js
@@ -1,1 +1,3 @@
-exports.run = (bot, msg, args) => {    msg.channel.send(`WRYYYYYYYYYYYYYYY!!! https://media.tenor.com/images/deb2616bf33fd36c1af6a9ac676d01c7/tenor.gif`).catch(console.error);}
+exports.run = (bot, msg, args) => {
+  msg.channel.send(`WRYYYYYYYYYYYYYYY!!! https://media.tenor.com/images/deb2616bf33fd36c1af6a9ac676d01c7/tenor.gif`).catch(console.error);
+}
